5731237f1cdb4e8463441e5dd30f838cafebcf04
moorelanguage <mooreanaemily@gmail.com>
2025-12-08 20:42:35 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), ghPages()],
+  plugins: [react(), tailwindcss()],
   base: '/',           
   build: {
     outDir: 'dist',      // output folder
